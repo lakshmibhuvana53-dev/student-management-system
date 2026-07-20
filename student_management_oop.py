7b999b99add5f6b2c5d188087967234260645933
lakshmibhuvana53-dev <lakshmibhuvana53@gmail.com>
2026-07-21 01:41:51 +0530

    the new methods are created

--- student_management_oop.py
+++ student_management_oop.py
@@ -14,15 +14,83 @@ class Student:
         print(f"Marks: {self.marks}")
 
 student1 = Student("Alice", "10th Grade", 1, 85)
-student2 = Student("Bob", "11th Grade", 2, 90)
-print("Student Information:")
-student1.display()
-student2.display()
+student2 = Student("Bob", "10th Grade", 2, 90)
 
-class Studentmanagementsystem:
+
+
+
+
+class StudentManagementSystem:
     def __init__(self):
-        self.students = [student1, student2]
+        self.students = []
     def add_student(self, student): 
+        for existing_student in self.students:
+            if student.roll_number == existing_student.roll_number:
+                print(f"Student with roll number {student.roll_number} already exists.")
+                return()
+            
         self.students.append(student)
+
+    def update_details(self):
+
+
+        given_reg_no = int(input("Enter the roll number: "))
+
+        for existing_student in self.students:
+
+            if given_reg_no == existing_student.roll_number:
+
+                field = input("What do you want to update?""\n(name/class_name/marks): ")
+                
+
+                new_value = input("Enter new value: ")
+
+                if hasattr(existing_student, field):
+                    setattr(existing_student, field, new_value)
+                    print("Student updated successfully.")
+                else:
+                    print("Invalid field.")
+
+                return
+
+        print("Student not found.") 
+
+    def search_student(self):
+        
+        given_reg_no = int(input("Enter the  roll_number of the student:  "))
+
+        for existing_student in self.students:
+
+            if given_reg_no == existing_student.roll_number:
+
+                (existing_student.display())
+
+                return
+
+                
+                
+                
+
                
+
+            
+
+        
+
+
+    
+
+        
+        
+
+sms = StudentManagementSystem()
+sms.add_student(student1)
+sms.add_student(student2)
+student3 = Student("Charlie", "10th Grade", 3, 85)
+sms.add_student(student3)
+sms.search_student()
+
+
+
+
 
