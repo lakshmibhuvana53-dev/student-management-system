be1aa1b378f89a9c825ee3ceb31b295239b868e3
lakshmibhuvana53-dev <lakshmibhuvana53@gmail.com>
2026-07-22 14:35:16 +0530

    the files are edited

--- student_management_oop.py
+++ student_management_oop.py
@@ -1,10 +1,16 @@
 class Student:
     def __init__(self, name, class_name, roll_number, marks):
+        
         self.name = name
+        
         self.class_name = class_name
+        
         self.roll_number = roll_number
+        
         self.marks = marks
+    
     def display(self):
+        
         print(f"Name: {self.name}")
 
         print(f"Class: {self.class_name}")
@@ -14,19 +20,22 @@ class Student:
         print(f"Marks: {self.marks}")
 
 student1 = Student("Alice", "10th Grade", 1, 85)
+
 student2 = Student("Bob", "10th Grade", 2, 90)
-
-
-
-
-
 class StudentManagementSystem:
+    
     def __init__(self):
+        
         self.students = []
+    
     def add_student(self, student): 
+        
         for existing_student in self.students:
+            
             if student.roll_number == existing_student.roll_number:
+                
                 print(f"Student with roll number {student.roll_number} already exists.")
+                
                 return()
             
         self.students.append(student)
@@ -63,32 +72,43 @@ class StudentManagementSystem:
 
             if given_reg_no == existing_student.roll_number:
 
-                (existing_student.display())
+                existing_student.display()
 
                 return
+        print("Student not found")
+
+    def delete_student(self):
+
+        given_reg_no = int(input("Enter the rol_number of the student: "))
+
+        for existing_student in self.students:
+
+            if given_reg_no == existing_student.roll_number:
+
+                existing_student.display()
+
+                field = input("Enter 'yes' to confirm or enter no: ").lower()
+
+                print(field)
+
+                if field == "yes":
+
+                    delattr(existing_student,field)
+
 
                 
-                
-                
-
-               
-
-            
-
-        
-
-
-    
-
-        
-        
 
 sms = StudentManagementSystem()
+
 sms.add_student(student1)
+
 sms.add_student(student2)
+ 
 student3 = Student("Charlie", "10th Grade", 3, 85)
+
 sms.add_student(student3)
-sms.search_student()
+
+sms.delete_student()
 
 
 
